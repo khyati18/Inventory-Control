ce8deea361ed6117c100fca3e5058e298b98017c
Anjali Deep <b18158@students.iitmandi.ac.in>
2020-11-15 19:16:39 +0530

    linked chemicals to wiki

--- inventorylist.php
+++ inventorylist.php
@@ -37,7 +37,6 @@
 	<tr>
 		<th>ID</th>
 		<th>Chemical Name</th>
-		<th>Wiki</th>
 		<th>Company Name</th>
 		<th>Price (per gm)</th>
 		<th>Quantity (in gm)</th>
@@ -50,7 +49,7 @@
 	$conn = OpenCon();
 	echo $conn->error;
 
-	$sql = "SELECT id, name, wiki, company, price FROM Items";
+	$sql = "SELECT id, name, company, price FROM Items";
 	$result = $conn->query($sql);
 
 	if ($result->num_rows > 0) 
@@ -63,7 +62,6 @@
 	    echo "<tr>";
 	    echo "<td>" . $row['id'] . "</td>";
 	    echo "<td>" . $row['name'] . "</td>";
-	    echo "<td>" . $row['wiki'] . "</td>";
 	    echo "<td>" . $row['company'] . "</td>";
 	    echo "<td>" . $row['price'] . "</td>";
 	    echo "<td>" . "<input  type='number' value='1' min='1' max='10000000' name='quantity_$count' />" . "</td>";
@@ -88,3 +86,4 @@
 
 </body>
 </html>
+
