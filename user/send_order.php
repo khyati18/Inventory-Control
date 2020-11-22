ef75d80f0dbe5d13dfc438b35bcf68ec5ab509cd
Jai Luthra <luthrajaiji@gmail.com>
2020-11-22 13:11:25 +0530

    Completed split-order

--- user/send_order.php
+++ user/send_order.php
@@ -7,11 +7,16 @@
 
     $details = $_GET['cart'];
     $remarks = $_POST['remark'];
-
     // echo $details;
+    $a = explode("<br>",$details);
 
-    $insert_q = "INSERT INTO Orders (UserId, OrderDetails, Remarks) VALUES (1 , '".$details."' , '".$remarks."')";
-    $result_insert = $conn->query($insert_q);
+    foreach ($a as $b){
+        if($b != "")
+        {
+            $insert_q = "INSERT INTO Orders (UserId, OrderDetails, Remarks) VALUES (1 , '".$b."' , '".$remarks."')";
+            $result_insert = $conn->query($insert_q);
+        }
+    }
 
     echo "<script>alert('Order Sent to Admin !'); window.history.go(-2);</script>";
 ?>
